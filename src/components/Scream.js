568990ba317fb4cb6e9e9e6d5204ec0022a62032
Spencer K-W <keashlywarters@gmail.com>
2019-07-23 17:07:15 -0400

    basic screams on homepage

---
+++ src/components/Scream.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Link from 'react-router-dom/Link';
+
+//MUI stuff
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    display: 'flex',
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200,
+    objectFit: 'cover'
+  },
+  content: {
+    padding: 25
+  }
+}
+
+export class Scream extends Component {
+  render() {
+    const {
+      classes,
+      scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount }
+    } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.image}
+          image={userImage}
+          title="Profile image"
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            color="primary"
+            component={Link}
+            to={`/users/${userHandle}`}
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+export default withStyles(styles)(Scream);
